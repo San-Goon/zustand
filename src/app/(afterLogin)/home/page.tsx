e3be1995ee5dcaf65fbe7001f0bec14510cf24b6
조강훈 <ganghun.jo@lgepartner.com>
2024-01-04 15:42:47 +0900

    feat: build postForm

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,4 +1,14 @@
-import styles from "./page.module.css";
+import styles from "./home.module.css";
+import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
 export default function Home() {
-  return <main className={styles.main}>home page</main>;
+  return (
+    <main className={styles.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+      </TabProvider>
+    </main>
+  );
 }
