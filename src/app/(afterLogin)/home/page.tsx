e894278f8e1d3577d9ea501a60704970c76e4c38
San-Goon <tigerlove95@naver.com>
2024-01-09 19:51:30 +0900

    feat: build post component

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,12 +2,17 @@ import styles from "./home.module.css";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Post from "@/app/(afterLogin)/_component/Post";
 export default function Home() {
   return (
     <main className={styles.main}>
       <TabProvider>
         <Tab />
         <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </TabProvider>
     </main>
   );
