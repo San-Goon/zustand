30e7ba5a2b9697ab249f1f2d33ce0383f082afd4
조강훈 <ganghun.jo@lgepartner.com>
2024-01-10 13:12:01 +0900

    feat: make search, explore pages

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,4 +1,16 @@
-import styles from "./page.module.css";
+import style from "./search.module.css";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/Trend";
 export default function Home() {
-  return <main className={styles.main}>search page</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+      </div>
+    </main>
+  );
 }
