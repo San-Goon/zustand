10855c3d272de5a360aba1c8fc27e956cc0966c6
조강훈 <ganghun.jo@lgepartner.com>
2024-01-10 15:53:45 +0900

    feat: make post, detail pages

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,4 +1,28 @@
-import styles from "./page.module.css";
-export default function Home() {
-  return <main className={styles.main}>id page</main>;
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import style from "./singlePost.module.css";
+import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm";
+
+export default function SinglePost() {
+  return (
+    <div className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      <Post />
+      <CommentForm />
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,15 +1,39 @@
 import style from "./search.module.css";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
-import Trend from "@/app/(afterLogin)/_component/Trend";
-export default function Home() {
+import Tab from "@/app/(afterLogin)/search/_component/Tab";
+import Post from "@/app/(afterLogin)/_component/Post";
+
+type Props = {
+  searchParams: { q: string; f?: string; pf?: string };
+};
+export default function Search({ searchParams }: Props) {
   return (
     <main className={style.main}>
-      <div className={style.formZone}>
-        <SearchForm />
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton />
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab />
       </div>
-      <div className={style.trend}>
-        <h3>나를 위한 트렌드</h3>
-        <Trend />
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
       </div>
     </main>
   );
