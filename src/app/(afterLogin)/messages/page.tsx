682a59ac253a12536dbdf4f9dbbceb15d8c450cb
조강훈 <ganghun.jo@lgepartner.com>
2024-01-11 12:35:58 +0900

    feat: make message components

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,4 +1,18 @@
-import styles from "./page.module.css";
+import style from "./message.module.css";
+import Room from "@/app/(afterLogin)/messages/_component/Room";
+
 export default function Home() {
-  return <main className={styles.main}>messages page</main>;
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
